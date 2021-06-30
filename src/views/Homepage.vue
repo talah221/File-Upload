8704badbc3b5ee5a97360333b4404beb37efa2d2
Tal Lahyani <tal@apoint.co.il>
2021-06-30 08:42:05 +0300

    ..

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,11 +1,27 @@
 <template>
-  <main class="container"></main>
+  <main class="container">
+    <FileUpload
+      name="demo[]"
+      url="./upload.php"
+      @upload="onUpload"
+      :multiple="true"
+      accept="image/*"
+      :maxFileSize="1000000"
+    >
+      <template #empty>
+        <p>Drag and drop files to here to upload.</p>
+      </template>
+    </FileUpload>
+  </main>
 </template>
 
 <script>
+import FileUpload from "primevue/fileupload";
+
 export default {
   name: "Homepage",
   directives: {},
+  components: { FileUpload },
   props: {},
   data() {
     return {};
